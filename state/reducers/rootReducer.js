3bb957fc4a292c72af1dff8814609235a79234a2
Ishwant Singh <ishwant8@gmail.com>
2019-10-20 00:53:47 +0530

    update: firebase connected.

--- state/reducers/rootReducer.js
+++ state/reducers/rootReducer.js
@@ -1,6 +1,25 @@
 import { combineReducers } from "redux";
 
+import firebase from "firebase";
+import "@firebase/firestore";
+
 import reducers from "./reducers";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYFzxYn4H0eQA7DKkuPwGItBLJfKz8Hug",
+  authDomain: "nutricheck.firebaseapp.com",
+  databaseURL: "https://nutricheck.firebaseio.com",
+  projectId: "nutricheck",
+  storageBucket: "nutricheck.appspot.com",
+  messagingSenderId: "918108625952",
+  appId: "1:918108625952:web:f6e20cb78bf3d29ec67045"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+console.log(db.collection("dishes").doc("1"), "boo");
 
 const rootReducer = combineReducers({
   reducers
